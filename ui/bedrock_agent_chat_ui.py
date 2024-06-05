8add468db6a2a3fde6b70da242ed9474bffaf7d4
Guy Ernest <guy.ernest@gmail.com>
2024-06-05 11:09:33 -0700

    Cleaning FastAPI code.

--- ui/bedrock_agent_chat_ui.py
+++ ui/bedrock_agent_chat_ui.py
@@ -4,7 +4,6 @@ from fastapi import FastAPI, Request, Form
 from fastapi.responses import FileResponse, HTMLResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
-from fastapi import Depends
 import json
 import uvicorn
 import boto3
@@ -22,13 +21,10 @@ app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
-# prettyprint json in jinja
-def ppjson(value, indent=2):
-	return json.dumps(json.loads(value), indent=indent)
-
-templates.env.filters['ppjson'] = ppjson
-
 bedrock_agent_runtime_client = boto3.client('bedrock-agent-runtime')
+agent_id = ssm.get_parameter(Name='/bedrock-agent-data/Bedrock-agent-id')['Parameter']['Value']
+agent_alias_id = ssm.get_parameter(Name='/bedrock-agent-data/Bedrock-agent-alias-id')['Parameter']['Value']
+
 
 @app.get("/use_case/questions", response_class=HTMLResponse, tags=['bedrock-agent'])
 async def switch_use_case(request: Request):
@@ -49,8 +45,6 @@ async def switch_use_case(request: Request):
 # Ask policy question on the org-shield directory
 @app.post("/chat-about-baseball", response_class=HTMLResponse, tags=['bedrock-agent'])
 async def ask_question(question: Annotated[str, Form()], request: Request):
-    agent_id = ssm.get_parameter(Name=f'/bedrock-agent-data/Bedrock-agent-id')['Parameter']['Value']
-    agent_alias_id = ssm.get_parameter(Name=f'/bedrock-agent-data/Bedrock-agent-alias-id')['Parameter']['Value']
 
     response = bedrock_agent_runtime_client.invoke_agent(
         agentAliasId=agent_alias_id,
